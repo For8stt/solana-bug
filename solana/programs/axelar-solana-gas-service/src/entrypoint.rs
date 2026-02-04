3c0f5b1718c72735fdb73578b54d98caa98e9e0b
For8stt <xkisil@stuba.sk>
2026-02-04 13:10:21 +0100

    remove

--- solana/programs/axelar-solana-gas-service/src/entrypoint.rs
+++
@@ -1,7 +0,0 @@
-//! Program entrypoint
-
-#![cfg(all(target_os = "solana", not(feature = "no-entrypoint")))]
-
-use crate::processor::process_instruction;
-
-solana_program::entrypoint!(process_instruction);
